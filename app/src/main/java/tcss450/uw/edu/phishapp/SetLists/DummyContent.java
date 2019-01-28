7cd358a629bb3d5c3002d1df06d2f43c84ef8ae4
Luke <lucasgillmore@yahoo.com>
2019-01-28 11:51:26 -0800

    Lab3 register may have bug

---
+++ app/src/main/java/tcss450/uw/edu/phishapp/SetLists/DummyContent.java
@@ -0,0 +1,72 @@
+package tcss450.uw.edu.phishapp.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample location for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<SetList> SET_LISTS = new ArrayList<SetList>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, SetList> SET_LIST_MAP = new HashMap<String, SetList>();
+
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    private static void addItem(SetList item) {
+        SET_LISTS.add(item);
+        SET_LIST_MAP.put(item.longDate, item);
+    }
+
+    private static SetList createDummyItem(int position) {
+        return new SetList(String.valueOf(position), "Item " + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore venue information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of location.
+     */
+    public static class SetList {
+        public final String longDate;
+        public final String location;
+        public final String venue;
+
+        public SetList(String longDate, String location, String venue) {
+            this.longDate = longDate;
+            this.location = location;
+            this.venue = venue;
+        }
+
+        @Override
+        public String toString() {
+            return location;
+        }
+    }
+}
---
+++ app/src/main/java/tcss450/uw/edu/phishapp/SetLists/SetList.java
@@ -0,0 +1,99 @@
+package tcss450.uw.edu.phishapp.dummy;
+
+import java.io.Serializable;
+
+public class SetList implements Serializable {
+
+    private final String mLongDate;
+    private final String mLocation;
+    private final String mVenue;
+    private final String mSetListData;
+    private final String mSetListNotes;
+    private final String mUrl;
+
+
+    public static class Builder {
+        private final String mLongDate;
+        private final String mLocation;
+        private  String mVenue = "";
+        private  String mSetListData = "";
+        private  String mSetListNotes = "";
+        private  String mUrl = "";
+
+
+        /**
+         *
+         * @param longDate
+         * @param location
+         */
+        public Builder(String longDate, String location) {
+            this.mLongDate = longDate;
+            this.mLocation = location;
+        }
+
+
+        public Builder addUrl(final String val) {
+            mUrl = val;
+            return this;
+        }
+
+        /**
+         * Add an optional teaser for the full blog post.
+         * @param venue an optional url teaser for the full blog post.
+         * @return the Builder of this BlogPost
+         */
+        public Builder addVenue(final String venue) {
+            mVenue = venue;
+            return this;
+        }
+
+        /**
+         * Add an optional author of the blog post.
+         * @param val an optional author of the blog post.
+         * @return the Builder of this BlogPost
+         */
+        public Builder addSetListData(final String val) {
+            mSetListData = val;
+            return this;
+        }
+
+        public Builder addSetListNotes(final String val) {
+            mSetListNotes = val;
+            return this;
+        }
+
+        public SetList build() {
+            return new SetList(this);
+        }
+
+    }
+
+    private SetList(final Builder builder) {
+        this.mLongDate = builder.mLongDate;
+        this.mLocation = builder.mLocation;
+        this.mVenue = builder.mVenue;
+        this.mSetListData = builder.mSetListData;
+        this.mSetListNotes = builder.mSetListNotes;
+        this.mUrl = builder.mUrl;
+    }
+
+    public String getLongDate() { return mLongDate;}
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public String getVenue() {
+        return mVenue;
+    }
+
+    public String getSetListData() {
+        return mSetListData;
+    }
+
+    public String getSetListNotes() {
+        return mSetListNotes;
+    }
+
+    public String getUrl(){ return mUrl;}
+}
